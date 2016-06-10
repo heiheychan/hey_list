97d8aa7db081d460165e1a93b7da26e2dd000836
heiheychan <cchei1993@gmail.com>
2016-06-09 23:14:57 -0400

    delete turbolinks to fix javascript unload issue

--- Gemfile
+++ Gemfile
@@ -7,7 +7,6 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
-gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
@@ -15,6 +14,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bootstrap-sass'
 gem 'devise'
 gem 'geocoder'
+gem 'js_cookie_rails'
 
 
 group :development, :test do
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_timezone 
+
+  def set_timezone  
+    min = request.cookies["time_zone"].to_i
+    puts min
+    Time.zone = ActiveSupport::TimeZone[-min]
+    puts Time.zone
+  end 
 end

63d869c38f4556b8d73f7fa57a245c3bf82ff073
heiheychan <cchei1993@gmail.com>
2016-06-13 02:22:40 -0400

    fix category search cookie bug & build user profile and update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require js.cookie
+//= require bootstrap-sprockets
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
+  root "posts#index"
+  # Devise Users and user-related pages
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
-
   devise_scope :user do
     get 'login', to: 'users/sessions#new'
     get 'signup', to: 'users/registrations#new'
   end
 
-  root "posts#index"
+  get 'profile', to: 'profile#index'
   resources "posts"
-
   # For Generating Sub-Category
   resources :categories do
     member do

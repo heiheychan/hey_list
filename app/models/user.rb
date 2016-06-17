284221a20ff4acf74319069086c4e5a328c954c7
heiheychan <cchei1993@gmail.com>
2016-06-17 15:25:35 -0400

    translate the index page to Chinese

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, password_length: 8..50
   validates :username, presence: true, length: {
                                         minimum: 1,
                                         maximum: 20

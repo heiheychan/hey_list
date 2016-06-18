50f70b99044dd3f0ad644e7fdb88b18166589bb5
heiheychan <cchei1993@gmail.com>
2016-06-17 21:19:21 -0400

    change default locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,12 @@ class ApplicationController < ActionController::Base
   # For additional Devise User attributes
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
 
+  def after_sign_in_path_for(resource)
+    current_user.wechat == nil || current_user.contact_email == nil || current_user.phone == nil ? edit_user_registration_path : root_path  
+  end
+
+  protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :username) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :username, :school, :major, :year_of_class, :bio, :wechat, :phone, :contact_email) }
